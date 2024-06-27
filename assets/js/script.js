7e956ab589fd3724de3ca333bf7b02f226937b2c
Harry Miles <harrypetermiles@gmail.com>
2024-06-27 15:40:15 +0000

    feat: add increment score and increment wrong answer tally functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ function checkAnswer() {
     console.log(calculatedAnswer[0], calculatedAnswer[1])
     if (isCorrect) {
         alert('Hey! You got it right.');
+        incrementScore();
     } else {
         alert(`Wrong answer! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+        incrementWrongAnswer();
     }
     console.log("test2");
     runGame(calculatedAnswer[1]);
@@ -69,12 +71,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current incorrect score tally from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
